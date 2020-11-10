9baefd3fd4a39bb38617a31a2db3bead094bff3d
Dm_Grt <grytsishin@gmail.com>
2020-11-11 00:08:10 +0200

    Controller removed, tests corrected

--- src/main/java/com/spring/boot/task/file/reading/CsvFileReaderImpl.java
+++ src/main/java/com/spring/boot/task/file/reading/CsvFileReaderImpl.java
@@ -9,12 +9,12 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FileReaderImpl implements FileReader {
+public class CsvFileReaderImpl implements FileReader {
     private static final String FILE_HEADER = "Id,ProductId,UserId,ProfileName,"
             + "HelpfulnessNumerator,HelpfulnessDenominator,Score,Time,Summary,Text";
     private final CSVReader reader;
 
-    public FileReaderImpl(CSVReader reader) {
+    public CsvFileReaderImpl(CSVReader reader) {
         this.reader = reader;
     }
 
@@ -23,14 +23,10 @@ public class FileReaderImpl implements FileReader {
         try (reader) {
             List<String[]> lines = new ArrayList<>();
             String[] nextLine;
-            while ((nextLine = reader.readNext()) != null) {
+            while ((nextLine = reader.readNext()) != null && lines.size() < 10) {
                 lines.add(nextLine);
             }
-            StringBuilder sb = new StringBuilder();
-            for (String s : lines.get(0)) {
-                sb.append(s).append(",");
-            }
-            if (FILE_HEADER.equals(sb.substring(0, sb.length() - 1))) {
+            if (isHeaderProvided(lines.get(0))) {
                 return lines;
             } else {
                 throw new NoHeaderException("File without header!");
@@ -40,5 +36,9 @@ public class FileReaderImpl implements FileReader {
         } catch (CsvValidationException e) {
             throw new RuntimeException("Can't validate Csv!", e);
         }
+    }
+
+    private boolean isHeaderProvided(String[] line) {
+        return FILE_HEADER.equals(String.join(",", line));
     }
 }
---
+++ src/test/java/com/spring/boot/task/file/reading/CsvFileReaderImplTest.java
@@ -0,0 +1,70 @@
+package com.spring.boot.task.file.reading;
+
+import com.opencsv.CSVReader;
+import com.spring.boot.task.exceptions.NoHeaderException;
+import java.io.FileNotFoundException;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class CsvFileReaderImplTest {
+    private static final int OK_LENGTH = 10;
+    private static final String INPUT_PATH_OK = "src/test/resources/Test.csv";
+    private static final String INPUT_PATH_WITHOUT_HEADER =
+            "src/test/resources/WithoutHeader.csv";
+    private static final String INPUT_PATH_BAD = "src/test/resources/TRe.csv";
+    private static final String FILE_HEADER =
+            "Id,ProductId,UserId,ProfileName,HelpfulnessNumerator,"
+                    + "HelpfulnessDenominator,Score,Time,Summary,Text";
+    private static final String LINE_FROM_FILE = "1,B001E4KFG0,A3SGXH7AUHU8GW,"
+            + "delmartian,1,1,5,1303862400,Good Quality Dog Food,I have bought "
+            + "several of the Vitality canned dog food products and have found "
+            + "them all to be of good quality. The product looks more like a stew "
+            + "than a processed meat and it smells better. My Labrador is finicky "
+            + "and she appreciates this product better than  most.";
+
+    @Test
+    public void readWithOkPath() throws FileNotFoundException {
+        List<String[]> resp;
+        FileReader reader = new CsvFileReaderImpl(new CSVReader(new java.io.FileReader(INPUT_PATH_OK)));
+        resp = reader.read();
+        Assert.assertEquals(resp.get(0).length, OK_LENGTH);
+    }
+
+    @Test
+    public void readWithBadPath() {
+        assertThrows(FileNotFoundException.class, () -> {
+            new CsvFileReaderImpl(new CSVReader(new java.io.FileReader(INPUT_PATH_BAD))).read();
+        });
+    }
+
+    @Test
+    public void isReadOk() throws FileNotFoundException {
+        FileReader reader = new CsvFileReaderImpl(new CSVReader(new java.io.FileReader(INPUT_PATH_OK)));
+        List<String[]> resp = reader.read();
+        String headerFromFile = String.join(",", resp.get(1));
+        assertEquals(LINE_FROM_FILE, headerFromFile);
+    }
+
+    @Test
+    public void fileWithHeader() throws FileNotFoundException {
+        FileReader reader = new CsvFileReaderImpl(new CSVReader(new java.io.FileReader(INPUT_PATH_OK)));
+        List<String[]> resp = reader.read();
+        StringBuilder sb = new StringBuilder();
+        for (String s: resp.get(0)) {
+            sb.append(s).append(",");
+        }
+        assertEquals(FILE_HEADER, sb.substring(0,sb.length() - 1));
+    }
+
+    @Test
+    public void fileWithoutHeader() {
+        assertThrows(NoHeaderException.class, () -> {
+            new CsvFileReaderImpl(new CSVReader(new java.io.FileReader(INPUT_PATH_WITHOUT_HEADER))).read();
+        });
+    }
+}
